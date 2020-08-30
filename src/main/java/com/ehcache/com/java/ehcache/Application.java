73c0313d4f6ff467343e30f141605b6a37461b81
Ajay Singh Meena <meenaajay1992@gmail.com>
2020-08-30 14:10:29 +0530

    initial commit

---
+++ src/main/java/com/ehcache/com/java/ehcache/Application.java
@@ -0,0 +1,13 @@
+package com.ehcache.com.java.ehcache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ehcache/com/java/ehcache/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ehcache.com.java.ehcache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
